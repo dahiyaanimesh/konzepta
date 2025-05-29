400b67bad8203a5ce175292c9c0405d3b91e6aff
liyab257 <liya.benoy257@gmail.com>
2025-05-29 22:54:26 +1000

    Update GenerateIdeasButton.jsx

--- src/components/GenerateIdeasButton.jsx
+++ src/components/GenerateIdeasButton.jsx
@@ -588,7 +588,7 @@ export default function GenerateIdeasButton() {
                     </ul>
 
                   </div>
-                  <ul style={{ listStyleType: 'none', paddingLeft: 0, marginLeft: '5px' }}>
+                  <ul style={{ listStyleType: 'none', paddingLeft: '5px', marginLeft: 0 }}>
                     {Array.isArray(gen.ideas) && gen.ideas.map((idea, ideaIdx) => (
                       <li key={ideaIdx} style={{ fontSize: '11px', marginBottom: '4px', backgroundColor: '#FFF68D', border: '1px solid #FFF68D', borderRadius: '6px', padding: '4px 10px 4px 10px', display: 'flex', alignItems: 'center', maxWidth: '300px', color: '#2E2E2E' }}>
                         {idea}
